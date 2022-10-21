246d1ad15f73e4d3ed8c69cc440ff06111c39623
Zlatko S <zstapic@foi.hr>
2022-10-21 16:26:54 +0200

    [Database] Define Store entity  - room entities are usually defined as data classes in Kotlin  - read about data classes in https://kotlinlang.org/docs/data-classes.html#properties-declared-in-the-class-body  - Store entity is defined in previously defined data model. E.g. here: https://user-images.githubusercontent.com/45765149/71315195-72807f80-2457-11ea-9c61-3807ff1297ed.png  - use proper annotations to annotate this entity. We used Entity and PrimaryKey. Notice the change of table name in relation to class name. If ommited, the table name would be as class name. Notice the use of auto increment parameter "autogenerate". This is important for the logic of adding the data into the database.  - define initial values for parameters of data class. Otherwise you can use a new constructor that calls super constructor with syntax "constructor():this(....)" in the body of the class.

--- database/src/main/java/hr/foi/air/database/entities/Store.kt
+++ database/src/main/java/hr/foi/air/database/entities/Store.kt
@@ -1,4 +1,15 @@
 package hr.foi.air.database.entities
 
-class Store {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "stores")
+data class Store(
+    @PrimaryKey(autoGenerate = true)
+    var id : Int? = null,
+    var name : String = "",
+    var description : String = "",
+    var imgUrl: String = "",
+    var latitude: Long = 0,
+    var longitude: Long = 0
+)

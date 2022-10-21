4ae4afc24ec427d2fadc4bb376b966e2db19b4ab
Zlatko S <zstapic@foi.hr>
2022-10-21 16:34:58 +0200

    [Database] Testing the database - mocking the data  - what about creating an object that will write some mock data into the database  - read about object and class declaration. e.g. here: https://stackoverflow.com/questions/44255946/difference-between-a-class-and-object-in-kotlin  - add stores and discounts as you wish  - notice the retreival of data from database  - notice the retreival of automatially defined id for new records and using it as a reference in foreign keys of discounts  - which line number would insert store into the database and with would insert discount?  - add call to mockSomeData after the application starts in MainActivity  - find local database in device explorer and explore its content with any SQLite editor tool that supports the databases stored in three files (some support only sqlite databses storeed in a single file).

--- app/src/main/java/hr/foi/air/dl/MainActivity.kt
+++ app/src/main/java/hr/foi/air/dl/MainActivity.kt
@@ -2,10 +2,18 @@ package hr.foi.air.dl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hr.foi.air.database.data.MockData
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mockSomeData()
+    }
+
+    fun mockSomeData()
+    {
+        MockData.mockData(this)
     }
 }

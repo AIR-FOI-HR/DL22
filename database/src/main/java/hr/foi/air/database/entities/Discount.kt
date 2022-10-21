37550dafd459d8113f740ad512212f2d3b4a0b0b
Zlatko S <zstapic@foi.hr>
2022-10-21 16:27:38 +0200

    [Database] Define Discount entity and handle foreign keys  - room entities are usually defined as data classes in Kotlin  - read about data classes in https://kotlinlang.org/docs/data-classes.html#properties-declared-in-the-class-body  - Discount entity is defined in previously defined data model. E.g. here: https://user-images.githubusercontent.com/45765149/71315195-72807f80-2457-11ea-9c61-3807ff1297ed.png  - use proper annotations to annotate this entity. We used Entity, PrimaryKey, ForeignKey and ColumnInfo. Notice the definition of primary and foreign keys. Notice the syntax for adding index.  - define initial values for parameters of data class. Notice the use of new instance of Date class which returns the current date and time.  - Notice that we have simple and complex data types. How the complex data types are stored in SQLite?

--- database/src/main/java/hr/foi/air/database/entities/Discount.kt
+++ database/src/main/java/hr/foi/air/database/entities/Discount.kt
@@ -1,4 +1,18 @@
 package hr.foi.air.database.entities
 
-class Discount {
-}
+import androidx.room.*
+import java.util.*
+
+@Entity(tableName = "discounts")
+data class Discount (
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
+    @ForeignKey(entity = Store::class, parentColumns = ["id"], childColumns = ["storeId"])
+    @ColumnInfo(index = true)
+    var storeId: Int? = null,
+    var name: String = "",
+    var description: String = "",
+    var discountValue: Int = 0,
+    var startDate: Date = Date(),
+    var endDate: Date = Date()
+)

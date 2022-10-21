7088d861e8ed5c7e6f6de939094bcb6b8ffae2ae
Zlatko S <zstapic@foi.hr>
2022-10-21 16:32:27 +0200

    [Database] Update main database class  - read about room and database class here: https://developer.android.com/training/data-storage/room  - make this class abstract  - extend RoomDatabase and call super constructor  - define abstract method to obtain one or more dao objects  - define annotation with version number, enumeration of entities and views. We don't need data base scheme to be exported as well.  - Notice the importance of version number. Read about updating database schemas and defining migrations here: https://developer.android.com/training/data-storage/room/migrating-db-versions and https://medium.com/androiddevelopers/understanding-migrations-with-room-f01e04b07929

--- database/src/main/java/hr/foi/air/database/MainDatabase.kt
+++ database/src/main/java/hr/foi/air/database/MainDatabase.kt
@@ -1,4 +1,12 @@
 package hr.foi.air.database
 
-class MainDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import hr.foi.air.database.entities.Discount
+import hr.foi.air.database.entities.Store
+import hr.foi.air.database.views.DiscountDetails
+
+@Database(version = 1, entities = [Store::class, Discount::class], views = [DiscountDetails::class], exportSchema = false)
+abstract class MainDatabase : RoomDatabase() {
+    abstract fun getDao(): DAO
 }

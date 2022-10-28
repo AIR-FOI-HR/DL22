fd8497684e81b6644b53108842a8da5f46b940d7
Zlatko S <zstapic@foi.hr>
2022-10-28 06:43:16 +0200

    [Simple data view] Add view binding  - Why is findViewById problematic?  - What about butter knife?  - What are specifics of view binding as introduced by google?

--- app/src/main/java/hr/foi/air/dl/MainActivity.kt
+++ app/src/main/java/hr/foi/air/dl/MainActivity.kt
@@ -2,12 +2,18 @@ package hr.foi.air.dl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import hr.foi.air.database.data.MockData
+import hr.foi.air.dl.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        //var btnShowData = findViewById<Button>(R.id.btn_show_data)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         mockSomeData()
     }

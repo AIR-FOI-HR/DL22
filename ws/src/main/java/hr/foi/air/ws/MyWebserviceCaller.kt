75ccf86c59ab491313c9de5f4b1097bd504f5511
Zlatko S <zstapic@foi.hr>
2022-11-04 07:35:38 +0100

    [Webservices] Implement a method to obtain discounts  - implement method by using the example from getAllStores  - notice the difference in json extraction as in this example the json contains a complex data type: date

--- ws/src/main/java/hr/foi/air/ws/MyWebserviceCaller.kt
+++ ws/src/main/java/hr/foi/air/ws/MyWebserviceCaller.kt
@@ -1,8 +1,11 @@
 package hr.foi.air.ws
 
+import com.google.gson.FieldNamingStrategy
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.squareup.okhttp.OkHttpClient
 import hr.foi.air.ws.responses.MyWebserviceResponse
+import hr.foi.air.core.entities.Discount
 import hr.foi.air.core.entities.Store
 import hr.foi.air.ws.handlers.MyWebserviceHandler
 import retrofit.*
@@ -37,6 +40,48 @@ class MyWebserviceCaller {
                                 response.body().items, Array<Store>::class.java)
                             dataArrivedHandler.onDataArrived<Store>(
                                 storeItems.toList(), true, response.body().timeStamp)
+
+                        }
+                        else {
+                            //TODO("Nešto nije u redu sa response")
+                        }
+                    }
+                    catch (ex: Exception) {
+                        //TODO("Iznimka u obradi odgovora")
+                    }
+                }
+
+                override fun onFailure(t: Throwable?) {
+                    TODO("Not yet implemented")
+                }
+            }
+        )
+    }
+
+    fun getAllDiscounts(method: String, dataArrivedHandler : MyWebserviceHandler)
+    {
+        val serviceAPI = retrofit.create(MyWebservice::class.java)
+        val call : Call<MyWebserviceResponse> = serviceAPI.getAllDiscounts(method)
+        call.enqueue (
+            object : Callback<MyWebserviceResponse>{
+                override fun onResponse(
+                    response: Response<MyWebserviceResponse>?,
+                    retrofit: Retrofit?
+                ) {
+                    try {
+                        if(response != null && response.isSuccess)
+                        {
+                            val customPolicy = FieldNamingStrategy {
+                                f -> f.name.replace("discountValue", "discount")
+                            }
+                            val gson = GsonBuilder()
+                                .setDateFormat("yyyy-MM-dd")
+                                .setFieldNamingStrategy(customPolicy)
+                                .create()
+                            val discountItems : Array<Discount> = gson.fromJson(
+                                response.body().items, Array<Discount>::class.java)
+                            dataArrivedHandler.onDataArrived<Discount>(
+                                discountItems.toList(), true, response.body().timeStamp)
 
                         }
                         else {

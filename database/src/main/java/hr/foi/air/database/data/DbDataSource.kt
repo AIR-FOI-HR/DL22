5c082e4256380ab592dade5bd1835cb56fb0a276
Zlatko S <zstapic@foi.hr>
2022-11-04 08:09:22 +0100

    [Modular data sources] Make DbDataSource implement interface  - Disconnect MainActivity from using DbDataSource directly  - Make DbDataSource implement DataSources interface  - Make DbDataSource convert data from database.entities to core.entities data types  - Make app and ws depend on core module

--- database/src/main/java/hr/foi/air/database/data/DbDataSource.kt
+++ database/src/main/java/hr/foi/air/database/data/DbDataSource.kt
@@ -1,10 +1,14 @@
 package hr.foi.air.database.data
 
 import android.content.Context
+import hr.foi.air.core.DataSource
+import hr.foi.air.core.DataSourceListener
+import hr.foi.air.core.entities.Discount
+import hr.foi.air.core.entities.Store
 import hr.foi.air.database.DAO
 import hr.foi.air.database.MainDatabase
 
-class DbDataSource(context: Context) {
+class DbDataSource(context: Context) : DataSource {
     var dao: DAO
 
     init {
@@ -13,5 +17,19 @@ class DbDataSource(context: Context) {
 
     fun getDiscountNames(): List<String> {
         return dao.getDiscountNames()
+    }
+
+    private fun getStores() : List<Store>
+    {
+        return dao.getAllStores()
+    }
+
+    private fun getDiscounts() : List<Discount>
+    {
+        return dao.getAllDiscounts()
+    }
+
+    override fun loadData(listener: DataSourceListener, contex: Context) {
+        listener.onDataLoaded(getStores(), getDiscounts())
     }
 }

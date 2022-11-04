e0361ae5841f7beba7f4b33dd29604dd95a0fe4c
Zlatko S <zstapic@foi.hr>
2022-11-04 07:19:37 +0100

    [Webservices] Define interface to call webservice and response structure  - simillar to room, retrofit will use your interface specification to automatically implemet a call object which will be capble of establishing a call to web service  - the call object needs to be aware of response that will be received from the webservice  - if necessary implement more than one response type. This depends on your api specification.

---
+++ ws/src/main/java/hr/foi/air/ws/responses/MyWebserviceResponse.kt
@@ -0,0 +1,8 @@
+package hr.foi.air.ws.responses
+
+class MyWebserviceResponse {
+    val items: String? = ""
+    val responseId: Int? = 0
+    val responseText: String? = ""
+    val timeStamp : Long? = 0
+}

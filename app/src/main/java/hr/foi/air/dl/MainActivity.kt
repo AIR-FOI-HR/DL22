d38ac6a6484b9fdbc73c51ec9fee78dedfaa2e9a
Zlatko S <zstapic@foi.hr>
2022-11-25 17:55:19 +0100

    [Navigation drawer] Drawer toggle and toolbar  - set your toolbar as support action bar  - initialize drawer toggle and add it to the toolbar  - sync drawer toggle and drawer  - note: drawer toggle is often refered to as hamburger icon

--- app/src/main/java/hr/foi/air/dl/MainActivity.kt
+++ app/src/main/java/hr/foi/air/dl/MainActivity.kt
@@ -1,6 +1,7 @@
 package hr.foi.air.dl
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import hr.foi.air.dl.data.DataRepository
 import hr.foi.air.dl.databinding.ActivityMainBinding
@@ -17,8 +18,22 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        initializeLayout()
         showMainFragment()
         loadDataToFragment()
+    }
+
+    private fun initializeLayout()
+    {
+        setSupportActionBar(binding.layoutMain.toolbar)
+        var drawerToggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.layoutMain.toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        binding.drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
     }
 
     private fun showMainFragment()
